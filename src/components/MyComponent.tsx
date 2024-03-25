373be532c5ce06a873b0562c598a09278ec99399
javier <javc2804@gmail.com>
2024-03-25 15:05:31 -0400

    inicial

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,5 @@
+const MyComponent: React.FC = () => {
+  return <div>MyComponent</div>;
+};
+
+export default MyComponent;
---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+const Routes: React.FC = () => {
+  return <Router></Router>;
+};
+
+export default Routes;
